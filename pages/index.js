8839f8a86872abd4007e23c5e288ed4d9ac56b67
LORRAN PORTO BESSA <202303219962@alunos.ibmec.edu.br>
2023-10-11 09:07:36 -0300

    index: minmal change

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
 import Image from 'next/image'
+import Fonts from '@/components/fonts'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
